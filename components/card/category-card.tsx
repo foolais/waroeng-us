b5db3479b4e560a0075d7c561aee5330b997763c
Wahyu Esya <wahyu.esya17@gmail.com>
2025-07-21 17:16:25 +0700

    fix(Cashier) handleClick filter category menu

--- components/card/category-card.tsx
+++ components/card/category-card.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import { useCashierFilterMenu } from "@/store/menu/useMenuFilter";
 import { Button } from "../ui/button";
+import { useCallback } from "react";
 
 const CategoryCard = ({ name }: { name: string }) => {
-  return <Button size="sm">{name}</Button>;
+  const { filter, setFilter } = useCashierFilterMenu();
+
+  const isActive = filter.type === name;
+
+  const handleClick = useCallback(() => {
+    setFilter({ type: isActive ? "" : name });
+  }, [isActive, name, setFilter]);
+
+  return (
+    <Button
+      size="sm"
+      variant={isActive ? "default" : "outline"}
+      onClick={handleClick}
+    >
+      {name}
+    </Button>
+  );
 };
 
 export default CategoryCard;
